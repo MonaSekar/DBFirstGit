10b07393314395d4ce1a796170c61ec16ea0d6a0
Mohana <mona7sekar@gmail.com>
2023-03-28 14:44:59 +0530

    first commit

---
+++ Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DBFirstGit.Models
+{
+    public partial class User
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int? Age { get; set; }
+        public int? LocationId { get; set; }
+
+        public virtual Location? Location { get; set; }
+    }
+}
