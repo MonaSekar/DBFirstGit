802dd140c0d35bacfe487af7c99f5819ea7802bb
BhaveshKumarReddy <Admin@DESKTOP-HHR16QE>
2023-03-28 15:10:06 +0530

    bhavesh first commit

---
+++ DbFirstGitCheck/Models/DbFirstGitCheckContext.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DbFirstGitCheck.Models
+{
+    public partial class DbFirstGitCheckContext : DbContext
+    {
+        public DbFirstGitCheckContext()
+        {
+        }
+
+        public DbFirstGitCheckContext(DbContextOptions<DbFirstGitCheckContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Location> Locations { get; set; } = null!;
+        public virtual DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-HHR16QE\\SQLEXPRESS;Database=DbFirstGitCheck;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Location>(entity =>
+            {
+                entity.ToTable("Location");
+
+                entity.Property(e => e.LocationName)
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.Location)
+                    .WithMany(p => p.Users)
+                    .HasForeignKey(d => d.LocationId)
+                    .HasConstraintName("FK__Users__LocationI__38996AB5");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ DbFirstGitCheck/Models/Location.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DbFirstGitCheck.Models
+{
+    public partial class Location
+    {
+        public Location()
+        {
+            Users = new HashSet<User>();
+        }
+
+        public int LocationId { get; set; }
+        public string? LocationName { get; set; }
+
+        public virtual ICollection<User> Users { get; set; }
+    }
+}
